a2109b2a99f1de430086faf855f421a9708959a3
Andrea Antonazzo <andrea.antonazzo@guest.telecomitalia.it>
2015-06-19 11:52:27 +0200

    Added Grizzly shutdown hook

--- SSEFrontEndApp/src/main/java/com/tilab/ca/ssefrontend/MainGrizzlyRunner.java
+++ SSEFrontEndApp/src/main/java/com/tilab/ca/ssefrontend/MainGrizzlyRunner.java
@@ -11,10 +11,12 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.aeonbits.owner.ConfigCache;
 import org.aeonbits.owner.event.ReloadEvent;
 import org.apache.log4j.Logger;
+import org.glassfish.grizzly.GrizzlyFuture;
 import org.glassfish.grizzly.http.server.accesslog.AccessLogBuilder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
@@ -51,17 +53,24 @@ public class MainGrizzlyRunner {
 
         sseConfigFromCache = ConfigCache.getOrCreate(SSEConfig.class);
         sseConfigFromCache.addReloadListener((ReloadEvent event) -> {
-            LOG.info("Reload intercepted at "+ new Date());
+            LOG.info("Reload intercepted at " + new Date());
         });
 
         LangDetectUtils.init();
-        
+
         HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(sseConfigFromCache.serviceUrl()), rc, false, null, false);
         enableAccessLog(httpServer);
+
+        // register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOG.info("shutdown hook invoked, stopping Grizzly server");
+            shutdownGrizzlyWebServer(httpServer);
+        }, "shutdownHook"));
+
         try {
             httpServer.start();
         } catch (Exception e) {
-            LOG.error( "could not start Grizzly server", e);
+            LOG.error("could not start Grizzly server", e);
             throw new RuntimeException(e);
         }
 
@@ -71,7 +80,6 @@ public class MainGrizzlyRunner {
         // exposing the Jersey application at configured serviceUrl 
         //return GrizzlyHttpServerFactory.createHttpServer(URI.create(sseConfigFromCache.serviceUrl()), rc);
     }
-    
 
     /**
      * Main method.
@@ -80,15 +88,15 @@ public class MainGrizzlyRunner {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        
+
         final HttpServer server = startServer();
-        
-        LOG.info("Jersey app started with WADL available at "+ sseConfigFromCache.serviceUrl());
+
+        LOG.info("Jersey app started with WADL available at " + sseConfigFromCache.serviceUrl());
         Thread.currentThread().suspend(); //XXX verify the best option to suspend the current thread
 
         server.stop();
     }
-    
+
     public static void enableAccessLog(HttpServer httpServer) {
         /*RotatingLogAppender appender = new RotatingLogAppender(System.out);
          AccessLogFormat format = ApacheLogFormat.COMBINED;
@@ -100,4 +108,23 @@ public class MainGrizzlyRunner {
         builder.instrument(httpServer.getServerConfiguration());
 
     }
+
+    protected static void shutdownGrizzlyWebServer(HttpServer grizzlyWebServer) {
+        if (grizzlyWebServer != null && grizzlyWebServer.isStarted()) {
+            // Gracefully stops the transport accepting new connections and allows existing work to complete before 
+            // finalizing the shutdown. This method will wait for the specified time for all interested parties to signal 
+            // it is safe to terminate the transport.
+            // If the timeout is exceeded, the transport will be terminated forcefully.
+            GrizzlyFuture<HttpServer> future = grizzlyWebServer.shutdown(10000, TimeUnit.MILLISECONDS);
+            //Returns true if this task completed
+            while (!future.isDone()) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignore) {
+                    LOG.error("ignore error = ", ignore);
+                }
+            }
+        }
+    }
+
 }
